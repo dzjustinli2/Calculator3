c08fa593e1b5a1c00ab5fb6ba5ffa0914c2f2901
dzjustinli2 <justin@justins-imac.gateway>
2016-07-08 12:19:09 +1000

    skeleton class and skeleton storyboard for calculator app

---
+++ Calculator3/CalculatorBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.swift
+//  Calculator3
+//
+//  Created by justin on 8/07/2016.
+//  Copyright © 2016 justin. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    
+}
---
+++ Calculator3/CalculatorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Calculator3
+//
+//  Created by justin on 8/07/2016.
+//  Copyright © 2016 justin. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var calculationStepsLabel: UILabel!
+    @IBAction func numberPressed(sender: UIButton) {
+    }
+
+    @IBAction func operationPressed(sender: UIButton) {
+    }
+}
+
